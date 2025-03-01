108db097faecb787b678e10014cb2d1af1ef7a75
Pedro Gonçalves <pedro0rsg@gmail.com>
2025-03-01 02:37:44 +0000

    - added point drag logic

---
+++ Assets/Scripts/Editor/Live2DDeformationTrackerEditor.Points.cs
@@ -0,0 +1,457 @@
+using Live2D.Cubism.Core;
+using UnityEditor;
+using UnityEngine;
+
+public sealed partial class Live2DDeformationTrackerEditor
+{
+    #region UI Constants
+
+    private const float HANDLE_SIZE = 0.015f;
+    private const float HANDLE_LINE_WIDTH = 4f;
+    private const float GUIDE_LINE_WIDTH = 4f;
+
+    private const float SMALL_POINT_SIZE_MULTIPLIER = 0.25f;
+    private const float CROSS_SIZE_MULTIPLIER = 0.35f;
+    private const float LABEL_POSITION_OFFSET_MULTIPLIER = 1.2f;
+
+    private static readonly Color k_HandleColorNormal = new Color(1f, 0.92f, 0.016f);
+    private static readonly Color k_HandleColorEdit = new Color(0.2f, 0.9f, 0.2f);
+    private static readonly Color k_HandleColorSelected = new Color(0.2f, 0.2f, 0.9f);
+
+    private static readonly Color k_GuideLineColor = new Color(1f, 1f, 1f, 0.75f);
+    private static readonly Color k_GuideLineConstraintColor = new Color(1f, 0.5f, 0.5f, 1f);
+    private static readonly Color k_GuideLineInactiveColor = new Color(1f, 1f, 1f, 0.5f);
+
+    #endregion
+
+    #region Point Handling
+
+    private void DrawAndHandlePoints(SceneView sceneView)
+    {
+        var tracker = Tracker;
+        var vertices = tracker.targetDrawable.VertexPositions;
+
+        using (new Handles.DrawingScope(tracker.transform.localToWorldMatrix))
+        {
+            for (int i = 0; i < tracker.trackedPoints.Length; i++)
+            {
+                var point = tracker.trackedPoints[i];
+                var position = CalculatePointPosition(tracker, point, i, vertices);
+
+                if (_isEditing)
+                {
+                    position = HandlePointInteraction(i, position, sceneView);
+                }
+
+                DrawPointVisuals(position, i, sceneView, _isEditing, tracker.enabled);
+            }
+        }
+    }
+
+    private Vector3 HandlePointInteraction(int index, Vector3 position, SceneView sceneView)
+    {
+        int controlID = GUIUtility.GetControlID(FocusType.Passive);
+
+        switch (Event.current.GetTypeForControl(controlID))
+        {
+            case EventType.KeyDown:
+                switch (Event.current.keyCode)
+                {
+                    case KeyCode.A:
+                        if (_isDragging && !_isAxisConstraintKeyPressed)
+                        {
+                            _isAxisConstrained = true;
+                            _constraintOrigin = _draggingPoint;
+                        }
+                        _isAxisConstraintKeyPressed = true;
+                        Event.current.Use();
+                        sceneView.Repaint();
+                        break;
+
+                    case KeyCode.G:
+                        _isGuidelineKeyPressed = true;
+                        if (_isDragging)
+                        {
+                            _constraintOrigin = _draggingPoint;
+                        }
+                        Event.current.Use();
+                        sceneView.Repaint();
+                        break;
+                }
+                break;
+
+            case EventType.KeyUp:
+                switch (Event.current.keyCode)
+                {
+                    case KeyCode.A:
+                        _isAxisConstraintKeyPressed = false;
+                        if (_isDragging)
+                        {
+                            _isAxisConstrained = false;
+                        }
+                        Event.current.Use();
+                        sceneView.Repaint();
+                        break;
+
+                    case KeyCode.G:
+                        _isGuidelineKeyPressed = false;
+                        Event.current.Use();
+                        sceneView.Repaint();
+                        break;
+                }
+                break;
+
+            case EventType.MouseDown:
+                if (HandleUtility.nearestControl == controlID && Event.current.button == 0)
+                {
+                    StartPointDrag(index, position, controlID);
+                    Event.current.Use();
+                }
+                break;
+
+            case EventType.MouseDrag:
+                if (GUIUtility.hotControl == controlID && _isDragging)
+                {
+                    position = UpdateDragPosition(position, sceneView);
+                    Event.current.Use();
+                }
+                break;
+
+            case EventType.MouseUp:
+                if (GUIUtility.hotControl == controlID && _isDragging)
+                {
+                    FinalizeDrag(index);
+                    Event.current.Use();
+                }
+                break;
+
+            case EventType.Layout:
+                HandleUtility.AddControl(
+                    controlID,
+                    HandleUtility.DistanceToCircle(position, HANDLE_SIZE * 0.5f)
+                );
+                break;
+
+            case EventType.Repaint:
+                UpdatePointSelection(index, controlID);
+                break;
+        }
+
+        return position;
+    }
+
+    private void StartPointDrag(int index, Vector3 position, int controlID)
+    {
+        _selectedPointIndex = index;
+        _isDragging = true;
+        _dragStartPosition = position;
+        _draggingPoint = position;
+        _isAxisConstrained = false;
+        _isAxisConstraintKeyPressed = false;
+        _isGuidelineKeyPressed = false;
+
+        GUIUtility.hotControl = controlID;
+    }
+
+    private Vector3 UpdateDragPosition(Vector3 position, SceneView sceneView)
+    {
+        Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        Plane plane = new Plane(Tracker.transform.forward, _dragStartPosition);
+
+        if (plane.Raycast(ray, out float distance))
+        {
+            Vector3 newPoint = ray.GetPoint(distance);
+
+            if (_isAxisConstraintKeyPressed && !_isAxisConstrained)
+            {
+                _isAxisConstrained = true;
+                _constraintOrigin = newPoint;
+            }
+            else if (!_isAxisConstraintKeyPressed && _isAxisConstrained)
+            {
+                _isAxisConstrained = false;
+            }
+
+            if (_isAxisConstrained)
+            {
+                var delta = newPoint - _constraintOrigin;
+                if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+                {
+                    newPoint.y = _constraintOrigin.y;
+                }
+                else
+                {
+                    newPoint.x = _constraintOrigin.x;
+                }
+            }
+
+            _draggingPoint = newPoint;
+            position = _draggingPoint;
+        }
+
+        sceneView.Repaint();
+        return position;
+    }
+
+    private void FinalizeDrag(int index)
+    {
+        Undo.RecordObject(Tracker, "Move Tracked Point");
+        Vector3 localPoint = Tracker.transform.InverseTransformPoint(_draggingPoint);
+        UpdatePointPosition(index, localPoint);
+
+        _isDragging = false;
+        _isAxisConstrained = false;
+        GUIUtility.hotControl = 0;
+        SceneView.RepaintAll();
+    }
+
+    private void UpdatePointSelection(int index, int controlID)
+    {
+        if (HandleUtility.nearestControl == controlID && GUIUtility.hotControl == 0)
+        {
+            _selectedPointIndex = index;
+        }
+        else if (GUIUtility.hotControl == 0 && _selectedPointIndex == index)
+        {
+            _selectedPointIndex = -1;
+        }
+    }
+
+    private void DrawPointVisuals(Vector3 position, int index, SceneView sceneView, bool isEditing, bool isTrackerEnabled)
+    {
+        if (_isDragging && _selectedPointIndex == index)
+        {
+            position = _draggingPoint;
+
+            if (_isAxisConstrained || _isGuidelineKeyPressed)
+            {
+                var point = _isGuidelineKeyPressed && !_isAxisConstrained
+                    ? _draggingPoint
+                    : _constraintOrigin;
+                DrawAxisGuidelines(point, sceneView);
+            }
+        }
+
+        Handles.color = GetPointColor(index);
+
+        if (!isTrackerEnabled && !isEditing)
+        {
+            DrawSimplePoint(position);
+        }
+        else
+        {
+            DrawFullHandle(position);
+        }
+
+        if (Event.current.alt)
+        {
+            DrawIndexLabel(position, index);
+        }
+    }
+
+    private Color GetPointColor(int index)
+    {
+        if (_selectedPointIndex == index)
+            return k_HandleColorSelected;
+
+        return _isEditing ? k_HandleColorEdit : k_HandleColorNormal;
+    }
+
+    private void DrawSimplePoint(Vector3 position)
+    {
+        float smallPointSize = HANDLE_SIZE * SMALL_POINT_SIZE_MULTIPLIER;
+        Handles.DrawSolidDisc(position, Vector3.forward, smallPointSize);
+    }
+
+    private void DrawFullHandle(Vector3 position)
+    {
+        Handles.DrawWireArc(position, Vector3.forward, Vector3.right, 360f, HANDLE_SIZE * 0.5f, HANDLE_LINE_WIDTH);
+
+        float crossSize = HANDLE_SIZE * CROSS_SIZE_MULTIPLIER;
+        Handles.DrawLine(
+            position + new Vector3(-crossSize, -crossSize, 0),
+            position + new Vector3(crossSize, crossSize, 0),
+            HANDLE_LINE_WIDTH
+        );
+        Handles.DrawLine(
+            position + new Vector3(-crossSize, crossSize, 0),
+            position + new Vector3(crossSize, -crossSize, 0),
+            HANDLE_LINE_WIDTH
+        );
+    }
+
+    private void DrawIndexLabel(Vector3 position, int index)
+    {
+        Vector3 labelPosition = position + Vector3.up * HANDLE_SIZE * LABEL_POSITION_OFFSET_MULTIPLIER;
+        Handles.Label(labelPosition, index.ToString(), k_LabelStyle);
+    }
+
+    private void DrawAxisGuidelines(Vector3 origin, SceneView view)
+    {
+        var originalColor = Handles.color;
+        var transform = Tracker.transform;
+
+        Vector3 originWorld = transform.TransformPoint(origin);
+        Vector3 originScreen = HandleUtility.WorldToGUIPoint(originWorld);
+
+        Plane plane = new Plane(transform.forward, originWorld);
+
+        var viewWidth = view.position.width;
+        var viewHeight = view.position.height;
+
+        var hStart = new Vector3(0, originScreen.y, 0);
+        var hEnd = new Vector3(viewWidth, originScreen.y, 0);
+        var vStart = new Vector3(originScreen.x, 0, 0);
+        var vEnd = new Vector3(originScreen.x, viewHeight, 0);
+
+        Vector3 hStartWorld = GetWorldPointOnPlane(hStart, plane);
+        Vector3 hEndWorld = GetWorldPointOnPlane(hEnd, plane);
+        Vector3 vStartWorld = GetWorldPointOnPlane(vStart, plane);
+        Vector3 vEndWorld = GetWorldPointOnPlane(vEnd, plane);
+
+        if (_isGuidelineKeyPressed && !_isAxisConstrained)
+        {
+            Handles.color = k_GuideLineColor;
+            Handles.DrawLine(hStartWorld, hEndWorld, GUIDE_LINE_WIDTH);
+            Handles.DrawLine(vStartWorld, vEndWorld, GUIDE_LINE_WIDTH);
+        }
+        else
+        {
+            var delta = _draggingPoint - _constraintOrigin;
+            bool isHorizontal = Mathf.Abs(delta.x) > Mathf.Abs(delta.y);
+
+            Handles.color = k_GuideLineConstraintColor;
+            if (isHorizontal)
+            {
+                Handles.DrawLine(hStartWorld, hEndWorld, GUIDE_LINE_WIDTH);
+                Handles.color = k_GuideLineInactiveColor;
+                Handles.DrawLine(vStartWorld, vEndWorld, GUIDE_LINE_WIDTH);
+            }
+            else
+            {
+                Handles.DrawLine(vStartWorld, vEndWorld, GUIDE_LINE_WIDTH);
+                Handles.color = k_GuideLineInactiveColor;
+                Handles.DrawLine(hStartWorld, hEndWorld, GUIDE_LINE_WIDTH);
+            }
+        }
+
+        Handles.color = originalColor;
+    }
+
+    private Vector3 GetWorldPointOnPlane(Vector3 screenPoint, Plane plane)
+    {
+        Ray ray = HandleUtility.GUIPointToWorldRay(screenPoint);
+        if (plane.Raycast(ray, out float distance))
+        {
+            return ray.GetPoint(distance);
+        }
+        return ray.origin;
+    }
+
+    private Vector3 CalculatePointPosition(Live2DDeformationTracker tracker, Live2DDeformationTracker.TrackedPoint point, int index, Vector3[] vertices)
+    {
+        if (Application.isPlaying)
+            return tracker.GetCurrentPosition(index);
+
+        return CalculateWeightedPosition(point, vertices);
+    }
+
+    private Vector3 CalculateWeightedPosition(Live2DDeformationTracker.TrackedPoint point, Vector3[] vertices)
+    {
+        Vector3 position = Vector3.zero;
+        var indices = point.trackingData.vertexIndices;
+        var weights = point.trackingData.weights;
+
+        for (int j = 0; j < 3; j++)
+        {
+            position += vertices[indices[j]] * weights[j];
+        }
+
+        return position;
+    }
+
+    private void UpdatePointPosition(int index, Vector3 localPosition)
+    {
+        if (index < 0 || index >= Tracker.trackedPoints.Length)
+            return;
+
+        Vector2 point2D = new Vector2(localPosition.x, localPosition.y);
+        var newData = CalculateBarycentricData(point2D, Tracker.targetDrawable);
+
+        var points = Tracker.trackedPoints;
+        points[index].trackingData = newData;
+
+        EditorUtility.SetDirty(Tracker);
+    }
+
+    private static Live2DDeformationTracker.BarycentricData CalculateBarycentricData(Vector2 point, CubismDrawable targetDrawable)
+    {
+        float minDistance = float.MaxValue;
+        Live2DDeformationTracker.BarycentricData bestData = default;
+
+        var vertices = targetDrawable.VertexPositions;
+        var indices = targetDrawable.Indices;
+
+        for (int i = 0; i < indices.Length; i += 3)
+        {
+            int i1 = indices[i];
+            int i2 = indices[i + 1];
+            int i3 = indices[i + 2];
+
+            var barycentricCoords = CalculateBarycentricCoordinates(
+                point, vertices[i1], vertices[i2], vertices[i3]);
+
+            bool isInside = barycentricCoords.x >= 0 && barycentricCoords.y >= 0 &&
+                           barycentricCoords.z >= 0 &&
+                           (barycentricCoords.x + barycentricCoords.y + barycentricCoords.z <= 1.001f);
+
+            if (isInside)
+            {
+                return new Live2DDeformationTracker.BarycentricData
+                {
+                    vertexIndices = new int[] { i1, i2, i3 },
+                    weights = barycentricCoords
+                };
+            }
+
+            var testPoint = vertices[i1] * barycentricCoords.x +
+                           vertices[i2] * barycentricCoords.y +
+                           vertices[i3] * barycentricCoords.z;
+            float distance = Vector3.Distance(point, testPoint);
+
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                bestData = new Live2DDeformationTracker.BarycentricData
+                {
+                    vertexIndices = new int[] { i1, i2, i3 },
+                    weights = barycentricCoords
+                };
+            }
+        }
+
+        return bestData;
+
+        static Vector3 CalculateBarycentricCoordinates(Vector2 point, Vector3 a, Vector3 b, Vector3 c)
+        {
+            Vector3 v0 = b - a;
+            Vector3 v1 = c - a;
+            Vector3 v2 = (Vector3)point - a;
+
+            float d00 = Vector3.Dot(v0, v0);
+            float d01 = Vector3.Dot(v0, v1);
+            float d11 = Vector3.Dot(v1, v1);
+            float d20 = Vector3.Dot(v2, v0);
+            float d21 = Vector3.Dot(v2, v1);
+
+            float denom = d00 * d11 - d01 * d01;
+            float v = (d11 * d20 - d01 * d21) / denom;
+            float w = (d00 * d21 - d01 * d20) / denom;
+            float u = 1.0f - v - w;
+
+            return new Vector3(u, v, w);
+        }
+    }
+
+    #endregion
+}
--- Assets/Scripts/Editor/Live2DDeformationTrackerEditor.cs
+++ Assets/Scripts/Editor/Live2DDeformationTrackerEditor.cs
@@ -1,55 +1,107 @@
-using Live2D.Cubism.Core;
 using UnityEditor;
 using UnityEditor.UIElements;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UIElements;
-using System.Linq;
 
 [CustomEditor(typeof(Live2DDeformationTracker))]
-public class Live2DDeformationTrackerEditor : Editor
+public sealed partial class Live2DDeformationTrackerEditor : Editor
 {
-    private const float HANDLE_SIZE = 0.015f;
-    private const float HANDLE_LINE_WIDTH = 4f;
+    #region UI Constants
 
-    private static readonly Color k_HandleColorNormal = new Color(1f, 0.92f, 0.016f);
-    private static readonly Color k_HandleColorEdit = new Color(0.2f, 0.9f, 0.2f);
+    private static readonly Color k_LabelBackgroundColor = new Color(0, 0, 0, 0.7f);
+    private static GUIStyle k_LabelStyle = null;
+
+    #endregion
+
+    #region Runtime State
 
     private VisualElement _root;
     private Button _editButton;
     private bool _isEditing;
+    private int _selectedPointIndex = -1;
+    private bool _isDragging;
+    private Vector3 _dragStartPosition;
+    private Vector3 _draggingPoint;
+    private bool _isAxisConstrained;
+    private Vector3 _constraintOrigin;
+    private bool _isAxisConstraintKeyPressed;
+    private bool _isGuidelineKeyPressed;
+
+    private bool IsExpanded => InternalEditorUtility.GetIsInspectorExpanded(target);
+    private Live2DDeformationTracker Tracker => target as Live2DDeformationTracker;
+
+    #endregion
+
+    #region Unity Methods
 
     public override VisualElement CreateInspectorGUI()
     {
         _root = new VisualElement();
 
-        // Load and clone the default USS
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/Editor/Live2DDeformationTrackerEditor.uss");
-        if (styleSheet != null)
-            _root.styleSheets.Add(styleSheet);
-
-        // Add the default inspector
-        var tracker = target as Live2DDeformationTracker;
-        var iterator = serializedObject.GetIterator();
-        bool enterChildren = true;
-
-        while (iterator.NextVisible(enterChildren))
-        {
-            enterChildren = false;
-
-            if (iterator.propertyPath == "trackedPoints")
-            {
-                // Add edit button before points array
-                CreateEditButton();
-            }
-
-            var propertyField = new PropertyField(iterator);
-            _root.Add(propertyField);
-        }
+        AddStyleSheet();
+        CreateInspectorElements();
 
         return _root;
+
+        void AddStyleSheet()
+        {
+            var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/Editor/Live2DDeformationTrackerEditor.uss");
+            if (styleSheet != null)
+                _root.styleSheets.Add(styleSheet);
+        }
+
+        void CreateInspectorElements()
+        {
+            var iterator = serializedObject.GetIterator();
+            bool enterChildren = true;
+
+            while (iterator.NextVisible(enterChildren))
+            {
+                enterChildren = false;
+
+                if (iterator.propertyPath == "trackedPoints")
+                {
+                    CreateEditModeButton();
+                }
+
+                var propertyField = new PropertyField(iterator);
+                _root.Add(propertyField);
+            }
+        }
     }
 
-    private void CreateEditButton()
+    private void OnEnable()
+    {
+        if (k_LabelStyle == null)
+            k_LabelStyle = CreateLabelStyle();
+
+        ResetEditorState();
+    }
+
+    private void OnDisable()
+    {
+        ResetEditorState();
+        SceneView.RepaintAll();
+    }
+
+    private void OnSceneGUI()
+    {
+        if (!IsExpanded)
+            return;
+
+        SetupSceneViewForEditing();
+        DrawAndHandlePoints(SceneView.currentDrawingSceneView);
+
+        if (_isEditing && Event.current.type == EventType.MouseMove)
+            SceneView.currentDrawingSceneView.Repaint();
+    }
+
+    #endregion
+
+    #region UI Setup
+
+    private void CreateEditModeButton()
     {
         var container = new VisualElement();
         container.style.marginBottom = 10;
@@ -65,90 +117,18 @@ public class Live2DDeformationTrackerEditor : Editor
         _root.Add(container);
     }
 
-    private void OnEnable()
-    {
-        // Subscribe to scene view events
-        SceneView.duringSceneGui += OnSceneGUIGlobal;
-
-        // Make sure we get priority for events
-        EditorApplication.update += CheckEditMode;
-    }
-
-    private void OnDisable()
-    {
-        // Turn off edit mode when disabled
-        _isEditing = false;
-
-        // Unsubscribe from scene view events
-        SceneView.duringSceneGui -= OnSceneGUIGlobal;
-        EditorApplication.update -= CheckEditMode;
-
-        SceneView.RepaintAll();
-    }
-
-    private void CheckEditMode()
-    {
-        if (_isEditing)
-        {
-            // Force the scene view to repaint to ensure our event handling is active
-            SceneView.RepaintAll();
-        }
-    }
-
-    private void OnSceneGUIGlobal(SceneView sceneView)
-    {
-        // Only intercept events when in edit mode
-        if (!_isEditing) return;
-
-        // Get the current event
-        var currentEvent = Event.current;
-
-        // Use our helper method to determine if we should capture this event
-        if (ShouldCaptureEvent(currentEvent))
-        {
-            // Prevent the event from being processed further
-            currentEvent.Use();
-
-            // Force a repaint to ensure our visuals stay updated
-            if (currentEvent.type == EventType.MouseMove)
-            {
-                SceneView.RepaintAll();
-            }
-        }
-    }
-
-    // Helper method to determine which events to capture
-    private bool ShouldCaptureEvent(Event evt)
-    {
-        // Capture left mouse button events (button 0)
-        if ((evt.type == EventType.MouseDown ||
-             evt.type == EventType.MouseUp ||
-             evt.type == EventType.MouseDrag) &&
-            evt.button == 0)
-        {
-            return true;
-        }
-
-        // Capture all keyboard events
-        if (evt.type == EventType.KeyDown ||
-            evt.type == EventType.KeyUp)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
     private void ToggleEditMode()
     {
         _isEditing = !_isEditing;
+        ResetEditorState();
         UpdateEditButtonState();
         SceneView.RepaintAll();
     }
 
     private void UpdateEditButtonState()
     {
-        if (_editButton == null) return;
+        if (_editButton == null)
+            return;
 
         _editButton.text = _isEditing ? "Done Editing" : "Edit Points";
 
@@ -162,108 +142,47 @@ public class Live2DDeformationTrackerEditor : Editor
         }
     }
 
-    private void OnSceneGUI()
+    private void ResetEditorState()
     {
-        var tracker = target as Live2DDeformationTracker;
-        if (!tracker || !tracker.enabled || tracker.targetDrawable == null) return;
-
-        // Hide the default transform tools when in edit mode
         if (_isEditing)
-        {
-            Tools.current = Tool.None;
+            return;
 
-            // Disable selection in the scene view
-            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
-        }
-
-        using (new Handles.DrawingScope(tracker.transform.localToWorldMatrix))
-        {
-            DrawTrackedPoints(tracker);
-        }
+        _selectedPointIndex = -1;
+        _isDragging = false;
     }
 
-    private void DrawTrackedPoints(Live2DDeformationTracker tracker)
+    private void SetupSceneViewForEditing()
     {
-        var vertices = tracker.targetDrawable.VertexPositions;
+        if (!_isEditing)
+            return;
 
-        // Draw all points
-        for (int i = 0; i < tracker.trackedPoints.Length; i++)
-        {
-            var point = tracker.trackedPoints[i];
-            if (point.trackingData.vertexIndices == null) continue;
-
-            Vector3 position = CalculatePointPosition(tracker, point, i, vertices);
-
-            // Determine handle color based on state
-            Color handleColor = _isEditing ? k_HandleColorEdit : k_HandleColorNormal;
-
-            DrawPointHandle(position, i, handleColor);
-        }
+        Tools.current = Tool.None;
+        HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
     }
 
-    private Vector3 CalculatePointPosition(Live2DDeformationTracker tracker, Live2DDeformationTracker.TrackedPoint point, int index, Vector3[] vertices)
+    private static GUIStyle CreateLabelStyle()
     {
-        if (Application.isPlaying)
+        var style = new GUIStyle(EditorStyles.miniLabel)
         {
-            return tracker.GetCurrentPosition(index);
-        }
+            normal = { textColor = Color.white },
+            fontSize = 12,
+            fontStyle = FontStyle.Bold,
+            alignment = TextAnchor.MiddleCenter,
+            padding = new RectOffset(5, 5, 2, 2)
+        };
 
-        Vector3 position = Vector3.zero;
-        var indices = point.trackingData.vertexIndices;
-        var weights = point.trackingData.weights;
+        style.normal.background = CreateLabelBackground();
 
-        for (int j = 0; j < 3; j++)
-        {
-            position += vertices[indices[j]] * weights[j];
-        }
-
-        return position;
+        return style;
     }
 
-    private void DrawPointHandle(Vector3 position, int index, Color color)
+    private static Texture2D CreateLabelBackground()
     {
-        Handles.color = color;
-
-        // Draw circle
-        Handles.DrawWireArc(position, Vector3.forward, Vector3.right, 360f, HANDLE_SIZE * 0.5f, HANDLE_LINE_WIDTH);
-
-        // Draw X
-        float crossSize = HANDLE_SIZE * 0.35f;
-        Handles.DrawLine(
-            position + new Vector3(-crossSize, -crossSize, 0),
-            position + new Vector3(crossSize, crossSize, 0),
-            HANDLE_LINE_WIDTH
-        );
-        Handles.DrawLine(
-            position + new Vector3(-crossSize, crossSize, 0),
-            position + new Vector3(crossSize, -crossSize, 0),
-            HANDLE_LINE_WIDTH
-        );
-
-        // Only draw index label if Alt key is pressed
-        if (Event.current.alt)
-        {
-            // Create a style with improved visibility
-            var style = new GUIStyle(EditorStyles.miniLabel)
-            {
-                normal = { textColor = Color.white },
-                fontSize = 12,
-                fontStyle = FontStyle.Bold,
-                alignment = TextAnchor.MiddleCenter,
-                padding = new RectOffset(5, 5, 2, 2)
-            };
-
-            // Add a background texture to the style
-            Texture2D backgroundTexture = new Texture2D(1, 1);
-            backgroundTexture.SetPixel(0, 0, new Color(0, 0, 0, 0.7f));
-            backgroundTexture.Apply();
-            style.normal.background = backgroundTexture;
-
-            // Position slightly above the handle
-            Vector3 labelPosition = position + Vector3.up * HANDLE_SIZE * 1.2f;
-
-            // Draw the label with the enhanced style
-            Handles.Label(labelPosition, index.ToString(), style);
-        }
+        Texture2D backgroundTexture = new Texture2D(1, 1);
+        backgroundTexture.SetPixel(0, 0, k_LabelBackgroundColor);
+        backgroundTexture.Apply();
+        return backgroundTexture;
     }
+
+    #endregion
 }
